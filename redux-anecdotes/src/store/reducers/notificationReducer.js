6d45e6f0748f19c61771703b9de947d1d4765f05
Xenia Cserkun <xenia.cserkun@gmail.com>
2021-11-30 11:55:27 +0100

    Second commit

---
+++ redux-anecdotes/src/store/reducers/notificationReducer.js
@@ -0,0 +1,56 @@
+/////////////////////////////// Action Creators
+const showNotifMessage = (msg) => ({
+  type: 'NOTIFICATION/SHOW_MESSAGE',
+  payload: { message: msg },
+});
+
+const hideNotifMessage = () => ({ type: 'NOTIFICATION/HIDE_MESSAGE' });
+
+/////////////////////////////// ASYNC action with Redux THUNK
+export const manageNotifAsync = (msg, sec) => {
+  return async (dispatch) => {
+    dispatch(showNotifMessage(msg));
+
+    setTimeout(() => {
+      dispatch(hideNotifMessage());
+    }, sec * 1000);
+  };
+};
+
+/////////////////////////////// REDUSER
+const initialState = {
+  notifMessage: 'Initial notification',
+  showNotification: false,
+};
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NOTIFICATION/ADD_VOTE': {
+      return {
+        ...state,
+        notifMessage: `You voted for ${action.payload.matchedObject.content}`,
+        showNotification: true,
+      };
+    }
+
+    case 'NOTIFICATION/SHOW_MESSAGE': {
+      return {
+        ...state,
+        notifMessage: action.payload.message,
+        showNotification: true,
+      };
+    }
+
+    case 'NOTIFICATION/HIDE_MESSAGE': {
+      return {
+        ...state,
+        showNotification: false,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
