cb62e386644990cf4992e16314ed92a0acc1bb70
XeniaChe <xeniacserkun@gmail.com>
2021-11-30 14:19:17 +0100

    master: anecdoteReducer cleaning

--- Part-6/redux-anecdotes/src/store/reducers/anecdoteReducer.js
+++ Part-6/redux-anecdotes/src/store/reducers/anecdoteReducer.js
@@ -59,27 +59,6 @@ export const addVoteAsync = (matchedAnecdote) => {
 };
 
 /////////////////////////////// REDUSER
-/* const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]; 
-
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-const getId = () => (100000 * Math.random()).toFixed(0);
-*/
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
@@ -89,22 +68,6 @@ const reducer = (state = initialState, action) => {
     }
 
     case 'ANECDOTES/ADD_VOTE': {
-      /*  
-      const copyState = [...state];
-      const matchedObject = copyState.find(
-        (anecdote) => anecdote.id === action.id
-        );
-        const anecdoteToUpdate = {
-          ...matchedObject,
-          votes: ++matchedObject.votes,
-        };
-      */
-
-      /*  const anecdoteToUpdate = {
-        ...action.matchedAnecdote,
-        votes: ++action.matchedAnecdote.votes,
-      }; */
-
       return state.map((anecdote) =>
         anecdote.id === action.matchedAnecdote.id
           ? action.matchedAnecdote
